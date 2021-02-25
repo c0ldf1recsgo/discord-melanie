81ff65ca558e5290f7d902f1d5ed1929cb3a8327
Hoàng Thịnh <shaynejohnson00@gmail.com>
2021-02-25 18:56:23 +0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -43,7 +43,7 @@ class Help(commands.Cog):
             embedVar.add_field(
             name="5. Hành động", value="slap, kiss, hug, pat", inline=False)
             embedVar.add_field(
-            name="6. Misc", value="snipe, quote, math, currency", inline=False)
+            name="6. Misc", value="avatar, snipe, quote, math, currency, translate", inline=False)
             embedVar.add_field(
             name="7. Settings", value="ping, prefix, nickname", inline=False)
             msg = await ctx.channel.send(embed=embedVar)
@@ -80,7 +80,7 @@ class Help(commands.Cog):
         elif args[0] == str(6) or args[0] == 'misc':
             embedVar = discord.Embed(
             title="Miscellaneous",
-            description="**Xem tin bị xóa:** snipe, spy.\nDùng `{0}help` `snipe` để xem chi tiết\n\n**Xem câu nói bất kì:** `{0}quote`.\nAliases: `quote`, `quotes`, `inspire`, `q`\n\n**Bot sủa:** `{0}say`.\nAliases: `s`\n\n**Bot sủa bằng emoji:** `{0}sayemo`.\nAliases: `se`\n\n**Chuyển đổi tiền tệ:** `{0}currency [số tiền] [trước] [sau]`.\nAliases: `cur`\n\n**Dịch:** `{0}translate [lang1]>[lang2] [trước] [sau]`.\nAliases: `tr`, `trans`".format(prefix),
+            description="**Xem avatar**- Của bản thân: `{0}avatar`.\n- Của người khác: `{0}avatar` + `[@user]`\nAliases: `avatar`, `ava`\n\n**Xem tin bị xóa:** snipe, spy.\nDùng `{0}help` `snipe` để xem chi tiết\n\n**Xem câu nói bất kì:** `{0}quote`.\nAliases: `quote`, `quotes`, `inspire`, `q`\n\n**Bot sủa:** `{0}say`.\nAliases: `s`\n\n**Bot sủa bằng emoji:** `{0}sayemo`.\nAliases: `se`\n\n**Chuyển đổi tiền tệ:** `{0}currency [số tiền] [trước] [sau]`.\nAliases: `cur`\n\n**Dịch:** `{0}translate [lang1]>[lang2] [trước] [sau]`.\nAliases: `tr`, `trans`".format(prefix),
             color=0x00ff00)
             msg = await ctx.channel.send(embed=embedVar)
         elif args[0] == str(7) or args[0] == 'settings':

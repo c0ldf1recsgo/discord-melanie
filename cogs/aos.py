daff3f3ea3b8fe38834684364c8203e6ae8a41e9
Hoàng Thịnh <shaynejohnson00@gmail.com>
2021-03-10 21:06:56 +0700

    Update aos.py

--- cogs/aos.py
+++ cogs/aos.py
@@ -2,13 +2,21 @@
 from datetime import datetime
 import pytz
 import random
+import json
 
 from discord.ext import commands, tasks
 
-# def get_prefix():
-#     with open('./cogs/prefixes.json', 'r') as f:
-#         prefix = json.load(f)
-#     return prefix['prefix']
+def get_prefix():
+    with open('./cogs/prefixes.json', 'r') as f:
+        prefix = json.load(f)
+    
+    return prefix['prefix']
+
+def get_bd():
+    with open('./cogs/prefixes.json', 'r') as f:
+        prefix = json.load(f)
+    
+    return prefix['bd']
 
 class AOS(commands.Cog):
 
@@ -22,7 +30,7 @@ class AOS(commands.Cog):
       self.good_morning.start()
       
       
-    @tasks.loop(seconds=86400)
+    @tasks.loop(seconds=3600)
     async def good_morning(self):
       dow = {'Mon': 'Thứ Hai', 'Tue': 'Thứ Ba', 'Wed':'Thứ Tư', 'Thu': 'Thứ Năm', 'Fri': 'Thứ Sáu', 'Sat': 'Thứ Bảy', 'Sun': 'Chủ Nhật'}
       t = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime('%a')
@@ -33,7 +41,7 @@ class AOS(commands.Cog):
       channel = self.client.get_channel(705598305857437696)
       # 797018066928009249 705598305857437696
       # await channel.send("test")
-      if (int(h) >= 5 and int(h) <= 7):
+      if (int(h) == 6):
         await channel.send(f'Buổi sáng vui vẻ nhé các bạn yêu :heart:.\nHôm nay là {dow[t]}, ngày {d} tháng {m} năm {y}')
         print('sent morning')
 
@@ -42,15 +50,16 @@ class AOS(commands.Cog):
     async def hpbd(self):
       bdquote = ['Bạn là một người bạn rất dễ thương! Chúc cho mọi sự như ý sẽ đến với bạn! Happy birthday to You! :partying_face:', 'Nhân dịp sinh nhật bạn nhé, chúc bạn luôn vui vẻ, thuận buồm xuôi gió trong công việc và hạnh phúc trong tình duyên. :partying_face:', 'Happy Birthday! Chúc bạn sinh nhật đầy ấm áp yêu thương và tiếng cười, thêm tuổi, thêm hạnh phúc, thêm nhiều niềm vui nhé. :partying_face:', 'Chúc bạn tuổi mới tiền vô như nước sức khỏe dồi dào. Sinh nhật vui vẻ. :partying_face:', 'Chẳng biết nói gì cả, chỉ biết chúc mừng sinh nhật bạn thôi :heart:, partytime :partying_face: :pinching_hand: :tada: ']
 
-      allab = db['bd']
+      allab = get_bd()
       a = []
       b = []
       for i in allab:
         a.append(i.split(' - ')[0])
         b.append(i.split(' - ')[1])
       today = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime('%d/%m')
+      h = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime('%H')
       # print(f'Today is {today}')
-      if today in b:
+      if (today in b) and (int(h) == 6):
         channel = self.client.get_channel(705598305857437696)
         for i in range(len(b)):
           if today == b[i]:
